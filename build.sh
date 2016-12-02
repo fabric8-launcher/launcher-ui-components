5c9d21433d75806c4e2529315e15ae313b4489cd
Charles Moulliard <cmoulliard@apache.org>
2016-12-02 15:23:15 +0100

    Update build.sh file to use one of the 2 strategies

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Docker Strategy
 oc delete imagestreams/front-generator
 oc delete bc/front-generator
 oc delete dc/front-generator
@@ -10,3 +11,15 @@ oc new-app https://github.com/obsidian-toaster/generator-frontend.git --name=fro
 
 oc env bc/front-generator FORGE_URL="http://generator-backend-default.192.168.64.71.xip.io/forge"
 
+# Source Strategy
+# oc create -f https://raw.githubusercontent.com/obsidian-toaster/generator-frontend/npm-install/imagestream.yaml
+# oc import-image nodejs-4
+# oc delete imagestreams/front-generator-s2i
+# oc delete bc/front-generator-s2i
+# oc delete dc/front-generator-s2i
+# oc delete service/front-generator-s2i
+# oc delete route/front-generator-s2i
+#
+# oc new-app nodejs-4~https://github.com/obsidian-toaster/generator-frontend.git#npm-install --name=front-generator-s2i --strategy=source
+#
+# oc env bc/front-generator-s2i FORGE_URL="http://generator-backend-default.192.168.64.72.xip.io/forge"
