d69b67d71d7fb4c309556bd9f29cff18bde8dea6
Charles Moulliard <cmoulliard@apache.org>
2016-12-03 18:31:19 +0100

    Add s2i scripts

--- build.sh
+++ build.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 
 # Using Template
-minishift delete
-minishift start --deploy-router=true --openshift-version=v1.3.1
-oc login --username=admin --password=admin
-eval $(minishift docker-env)
-oc delete template/front-agenerator
-oc create -f template.yml
-oc process front-generator FORGE_URL=http://generator-backend-default.192.168.64.75.xip.io/forge | oc create -f -
-oc start-build front-generator
+# minishift delete
+# minishift start --deploy-router=true --openshift-version=v1.3.1
+# oc login --username=admin --password=admin
+# eval $(minishift docker-env)
 
-# Docker Strategy
 oc delete is/node
+oc delete is/node-6
 oc delete is/front-generator
 oc delete bc/front-generator
 oc delete dc/front-generator
 oc delete service/front-generator
 oc delete route/front-generator
+oc delete template/front-generator
+#oc create -f templates/template_docker.yml
+oc create -f templates/template_s2i.yml
+oc process front-generator FORGE_URL=http://generator-backend-default.192.168.64.75.xip.io/forge | oc create -f -
+oc start-build front-generator
 
-oc new-app https://github.com/obsidian-toaster/generator-frontend.git --name=front-generator --strategy=docker
-
-oc env bc/front-generator FORGE_URL="http://generator-backend-default.192.168.64.73.xip.io/forge"
-oc expose svc/front-generator
+# Docker Strategy
+# oc delete is/node
+# oc delete is/front-generator
+# oc delete bc/front-generator
+# oc delete dc/front-generator
+# oc delete service/front-generator
+# oc delete route/front-generator
+#
+# oc new-app https://github.com/obsidian-toaster/generator-frontend.git --name=front-generator --strategy=docker
+#
+# oc env bc/front-generator FORGE_URL="http://generator-backend-default.192.168.64.73.xip.io/forge"
+# oc expose svc/front-generator
 
 # Source Strategy
 # oc create -f https://raw.githubusercontent.com/obsidian-toaster/generator-frontend/npm-install/imagestream.yaml
