e4a112d5546ec04b27d92790a59b509cb2c81eef
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-12-07 14:17:00 +0100

    fixed url

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,7 +16,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 # Save some useful information
-REPO=`https://github.com/obsidian-toaster/obsidian-toaster.github.io`
+REPO=https://github.com/obsidian-toaster/obsidian-toaster.github.io.git
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
