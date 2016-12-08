80419a958d2705520ce2c16549baa4d9cec12611
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-12-08 15:28:51 +0100

    fix header padding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
-  styles: [
-    "../../node_modules/patternfly/dist/css/patternfly.min.css",
-    "../../node_modules/patternfly/dist/css/patternfly-additions.min.css",
-    './app.component.scss'
-  ]
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 }
