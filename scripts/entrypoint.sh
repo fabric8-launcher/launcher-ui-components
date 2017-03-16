5bb8a441369ad2ec9d31e1174f25239e60304b1b
Václav Pavlín <vaclav.pavlin@gmail.com>
2017-03-16 13:25:02 +0100

    Remove default OpenSHift env var

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,10 +2,6 @@
 
 SETTINGS="/usr/share/nginx/html/settings.json"
 
-if [ -n "${OBSIDIAN_BACKEND_SERVICE_HOST}" ]; then
-   BACKEND_URI="http://${OBSIDIAN_BACKEND_SERVICE_HOST}:${OBSIDIAN_BACKEND_SERVICE_PORT}/"
-fi
-
 if [ -n "${BACKEND_URI}" ]; then
     sed -i.bckp 's#"backend_url": ".*"#"backend_url": "'${BACKEND_URI}'"#' ${SETTINGS}
 fi
