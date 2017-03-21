2f31cce1a876f9256e3ca0f3fa5593900c6bfa53
Tako Schotanus <tako@ceylon-lang.org>
2017-03-21 19:12:01 +0100

    Changed `BACKEND_URI` to `BACKEND_URL` to be consistent with its use in other parts of the code

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,8 +2,8 @@
 
 SETTINGS="/usr/share/nginx/html/settings.json"
 
-if [ -n "${BACKEND_URI}" ]; then
-    sed -i.bckp 's#"backend_url": ".*"#"backend_url": "'${BACKEND_URI}'"#' ${SETTINGS}
+if [ -n "${BACKEND_URL}" ]; then
+    sed -i.bckp 's#"backend_url": ".*"#"backend_url": "'${BACKEND_URL}'"#' ${SETTINGS}
 fi
 
 exec /run.sh
