dc14231edfbf45c1e1a10a4b84af4f361f20d381
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-03-24 13:21:48 +0100

    fixed location

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 SETTINGS="/usr/share/nginx/html/settings.json"
+APP_JS="/usr/share/nginx/html/app*.js"
 
 if [ -n "${BACKEND_URL}" ]; then
     sed -i.bckp 's#"backend_url": ".*"#"backend_url": "'${BACKEND_URL}'"#' ${SETTINGS}
 fi
 
 if [ -n "${KEYCLOAK_SKIP}"]; then
-    sed -i.bckp 's/keycloakSkip:!./keycloakSkip:'${KEYCLOAK_SKIP}'/g' app*.js
+    sed -i.bckp 's/keycloakSkip:!./keycloakSkip:'${KEYCLOAK_SKIP}'/g' ${APP_JS}
 fi
 
 exec /run.sh
