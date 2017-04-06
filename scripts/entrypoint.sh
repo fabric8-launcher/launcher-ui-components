a211f65203cf210848de8e6f1df07ac89c3fa41c
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-04-06 22:01:34 +0200

    no longer needed now checks if realm is set

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,10 +7,6 @@ if [ -n "${LAUNCHPAD_BACKEND_URL}" ]; then
     sed -i.bckp 's#"backend_url": ".*"#"backend_url": "'${LAUNCHPAD_BACKEND_URL}'"#' ${SETTINGS}
 fi
 
-if [ -n "${LAUNCHPAD_KEYCLOAK_SKIP}" ]; then
-    sed -i.bckp 's/keycloakSkip:!./keycloakSkip:'${LAUNCHPAD_KEYCLOAK_SKIP}'/g' ${APP_JS}
-fi
-
 if [ -n "${LAUNCHPAD_KEYCLOAK_URL}" ]; then
     sed -i.bckp 's#realm:.*,clientId#realm:"'${LAUNCHPAD_KEYCLOAK_REALM}'",url:"'${LAUNCHPAD_KEYCLOAK_URL}'",clientId#' ${APP_JS}
 fi
