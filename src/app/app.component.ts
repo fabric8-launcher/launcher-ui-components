a1ca5840cb9434aa05d5676195a1858c0065f3ae
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-04-20 14:46:48 +0200

    moved background style to app component
    do not repeat
    
    better background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  host: {
+    "[class.intro]":"intro"
+  }
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    router.events.subscribe((url:any) => {
+      this.intro = url.url == '/' || url.url == '/wizard';
+    });
+  }
+
+  intro: boolean;
 }
