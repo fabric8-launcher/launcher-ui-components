22a35634245e6597a158654fa3f1461875608f6f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-04-21 19:39:52 +0200

    index

--- src/app/wizard/intro/intro.component.ts
+++ src/app/wizard/intro/intro.component.ts
@@ -10,6 +10,6 @@ export class IntroComponent {
   constructor(private router: Router) {}
 
   launch() {
-    this.router.navigate(['/wizard', "launchpad-new-project", 0, ""]);
+    this.router.navigate(['/wizard', "launchpad-new-project", 1, ""]);
   }
 }
