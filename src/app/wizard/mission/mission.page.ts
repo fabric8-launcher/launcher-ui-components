d910facfb2cf78acd4d8a74da47618be332acd7c
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-05-04 15:07:49 +0200

    added login

--- src/app/wizard/mission/mission.page.ts
+++ src/app/wizard/mission/mission.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { Gui } from "../../shared/model";
+import { KeycloakService } from "../../shared/keycloak.service";
 
 @Component({
   selector: "mission",
@@ -7,5 +8,8 @@ import { Gui } from "../../shared/model";
 })
 export class MissionPage {
   @Input() gui: Gui;
+  @Input() authenticationNeeded: boolean;
+
+  constructor(private keycloak: KeycloakService) {}
 }
 
