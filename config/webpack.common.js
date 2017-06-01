b5e3fe85efc554647546675503bca95967a182c5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-06-01 22:08:16 +0200

    update versions (#132)

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -82,7 +82,7 @@ module.exports = {
      */
     new webpack.ContextReplacementPlugin(
       // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      /angular(\\|\/)core(\\|\/)@angular/,
       helpers.root('src') // location of your src
     ),
 
