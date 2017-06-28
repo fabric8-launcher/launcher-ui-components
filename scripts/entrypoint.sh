6ba4de582f3190c1cbcd59d2572365fa25af42f0
Tako Schotanus <tako@ceylon-lang.org>
2017-06-28 14:13:41 +0200

    Fixed #130 Take Segment tracking key from env var

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 
 SETTINGS="/usr/share/nginx/html/settings.json"
 APP_JS="/usr/share/nginx/html/app*.js"
+INDEX="/usr/share/nginx/html/index.html"
 
 if [ -n "${LAUNCHPAD_BACKEND_URL}" ]; then
     sed -i.bckp 's#"backend_url": ".*"#"backend_url": "'${LAUNCHPAD_BACKEND_URL}'"#' ${SETTINGS}
@@ -13,6 +14,14 @@ fi
 
 if [ -n "${LAUNCHPAD_KEYCLOAK_URL}" ]; then
     sed -i.bckp 's#realm:.*,clientId#realm:"'${LAUNCHPAD_KEYCLOAK_REALM}'",url:"'${LAUNCHPAD_KEYCLOAK_URL}'",clientId#' ${APP_JS}
+fi
+
+if [ -n "${LAUNCHPAD_TRACKER_SEGMENT_TOKEN}" ]; then
+    sed -i.bckp "s#{SegmentTrackerToken}#${LAUNCHPAD_TRACKER_SEGMENT_TOKEN}#" ${INDEX}
+else
+    # This is a pretty flaky way to remove the analytics script, any change to
+    # the original script most likely will need changes here as well
+    sed -i.bckp 's#<script>!function.*}}..<\/script>##' ${INDEX}
 fi
 
 exec /run.sh
