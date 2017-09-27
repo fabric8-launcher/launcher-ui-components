fb13726992cedfa207ebdc14da7c11314b70ee37
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-09-27 17:25:24 +0200

    moved prefix out of input box

--- src/app/wizard/components/project-name-input/project-name-input.component.ts
+++ src/app/wizard/components/project-name-input/project-name-input.component.ts
@@ -1,8 +1,10 @@
 import { Component, forwardRef, NgModule } from "@angular/core";
-import { InputComponent } from "../input/input.component";
-import { FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
-import { KeycloakService } from "../../../shared/keycloak.service";
 import { CommonModule } from "@angular/common";
+import { FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
+
+import { InputComponent } from "../input/input.component";
+import { Input } from "../../../shared/model";
+import { KeycloakService } from "../../../shared/keycloak.service";
 import { SuggestFilterPipe } from "./filter.pipe";
 
 @Component({
@@ -18,18 +20,32 @@ import { SuggestFilterPipe } from "./filter.pipe";
   ]
 })
 export class ProjectNameInputComponent extends InputComponent {
+  projectName: string = "";
+  prefix: string;
   constructor(private keycloak: KeycloakService) {
     super();
+    this.createPrefix();
     this.keyUp.subscribe(_ => {
-      if (this.input.value && this.input.value.indexOf(this.keycloak.username()) === -1) {
-        this.input.value = this.keycloak.username() + '-' + this.input.value;
-      }
+      this.input.value = this.keycloak.username() + '-' + this.projectName;
+      this.createPrefix();
     });
+  }
+
+  private createPrefix() {
+    this.prefix = this.keycloak.username() + ' -';
   }
 
   setValue(value: string) {
     this.input.value = value;
-    this.onModelChange();
+    this.projectName = value;
+    this.prefix = "";
+    this.onModelChange(value);
+  }
+
+  writeValue(obj: Input): void {
+    if (obj && obj.value) {
+      this.input.value = this.projectName + obj.value;
+    }
   }
 }
 
