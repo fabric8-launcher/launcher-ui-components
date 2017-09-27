67d76dd02867f454c7fdce6d96390d3b608af624
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-09-27 07:44:07 +0200

    prefix openShift project name with username (#191)

--- src/app/wizard/pages/projectInfo/projectInfo.page.ts
+++ src/app/wizard/pages/projectInfo/projectInfo.page.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { GenericPage } from "../generic/generic.page";
 import { SubmittableInput } from "../../../shared/model";
+import {KeycloakService} from "../../../shared/keycloak.service";
 
 @Component({
   selector: "projectInfo",
@@ -8,6 +9,10 @@ import { SubmittableInput } from "../../../shared/model";
 })
 export class ProjectInfoPage extends GenericPage {
   expand: boolean;
+
+  constructor(private keycloak: KeycloakService) {
+    super();
+  }
 
   toggle() {
     this.expand = !this.expand;
@@ -19,6 +24,10 @@ export class ProjectInfoPage extends GenericPage {
 
   modelChanged(value: any) {
     this.validate.emit();
+    let named = this.getField("named");
+    if (named.value && named.value.indexOf(this.keycloak.username()) === -1) {
+      named.value = this.keycloak.username() + '-' + named.value;
+    }
     if (value) {
       let gitHubRepositoryName = this.getField("gitHubRepositoryName");
       gitHubRepositoryName.value = value;
