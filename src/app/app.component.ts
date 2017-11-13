cb338cb571d66e5430a9bdacd17cf78c07bffca2
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-11-13 16:46:32 +0100

    improve analytics and page load time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {Router} from "@angular/router";
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: "body",
@@ -13,6 +13,15 @@ export class AppComponent {
   constructor(private router: Router) {
     router.events.subscribe((url: any) => {
       this.intro = url.url !== "/" && url.url !== "/wizard";
+    });
+
+    router.events.distinctUntilChanged((previous: any, current: any) => {
+      if (current instanceof NavigationEnd) {
+        return previous.url === current.url;
+      }
+      return true;
+    }).subscribe((x: any) => {
+      window['analytics'].page();
     });
   }
 
