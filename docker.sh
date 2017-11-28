c45c07802587e74640a6c8064a3ff439477027f6
Tako Schotanus <tako@ceylon-lang.org>
2017-11-28 23:51:03 +0100

    Make the frontend's nginx handle the proxying to the backend

--- docker.sh
+++ docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# this is for when we run without the "--net" option.
+# Nginx doens't like it when it can't reach the launcher-backend
+# and will fail to start. So we add a hosts entry of our own
+# pointing to the docker host
+DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+EXTRA_OPTS="--add-host launcher-backend:$DOCKER_HOST"
+
 # see if a "--net" option was passed, if so we'll connect the
 # container to a private network (creating it if necessary)
 NETWORK=default
@@ -12,6 +19,7 @@ for arg; do
                 # override the connection URLs for the application
                 LAUNCHPAD_BACKEND_URL=http://localhost:8088/launch/api
                 LAUNCHPAD_MISSIONCONTROL_URL=ws://localhost:8088/launch
+                EXTRA_OPTS=""
                 ;;
         *)  DRUN_OPTS="$DRUN_OPTS ${arg}"
                 ;;
@@ -38,6 +46,7 @@ docker run \
     -eLAUNCHPAD_MISSIONCONTROL_URL=$LAUNCHPAD_MISSIONCONTROL_URL \
     -eLAUNCHPAD_TRACKER_SEGMENT_TOKEN=$LAUNCHPAD_TRACKER_SEGMENT_TOKEN \
     $DRUN_OPTS \
+    $EXTRA_OPTS \
     fabric8/launcher-frontend
 
 
