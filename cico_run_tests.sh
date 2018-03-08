ae5e7e1dc2364aae7b9183073661dc2e55fc0aab
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-03-08 09:43:01 +0100

    chore (build) fix bash script

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Show command before executing
-set -x
-
-# Exit on error
-set -e
-
 # Export needed vars
 set +x
 for var in BUILD_NUMBER BUILD_URL JENKINS_URL GIT_BRANCH GH_TOKEN NPM_TOKEN GIT_COMMIT DEVSHIFT_USERNAME DEVSHIFT_PASSWORD DEVSHIFT_TAG_LEN; do
   export $(grep ${var} jenkins-env | xargs)
 done
 export BUILD_TIMESTAMP=`date -u +%Y-%m-%dT%H:%M:%S`+00:00
+
+# Show command before executing
 set -x
+set -e
 
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0
@@ -39,8 +36,7 @@ fi
 mkdir -p dist
 docker run --detach=true --name=launcher-frontend-builder -t -v $(pwd)/dist:/dist:Z ${PULLREGISTRY}/fabric8/launcher-frontend-builder:${TAG}
 
-ret=$?
-if [ $ret -ne 0 ]; then
+if [ $? -ne 0 ]; then
   docker build -t launcher-frontend-builder -f Dockerfile.build . && \
   docker tag launcher-frontend-builder ${REGISTRY}/fabric8/launcher-frontend-builder:${TAG} && \
   docker push ${REGISTRY}/fabric8/launcher-frontend-builder:${TAG}
