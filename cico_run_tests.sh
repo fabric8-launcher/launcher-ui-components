c0925fa684a76f37cdf238d1358a92e6f26cf682
George Gastaldi <gegastaldi@gmail.com>
2018-03-11 22:26:12 -0300

    fix(tests): Fix PR CI tests

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -1,47 +1,39 @@
 #!/bin/bash
 
+# Exit on error
 set -e
 
-NODE_VERSION=6.9.2
+BUILDER_IMAGE="launcher-frontend-builder"
+BUILDER_CONT="launcher-frontend-builder-container"
+DEPLOY_IMAGE="launcher-frontend-deploy"
+TARGET_DIR="dist"
 
-# gpg keys listed at https://github.com/nodejs/node
-set -ex \
-  && for key in \
-    9554F04D7259F04124DE6B476D5A82AC7E37093B \
-    94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
-    0034A06D9D9B0064CE8ADF6BF1747F4AD2306D93 \
-    FD3A5288F042B6850C66B31F09FE44734EB7990E \
-    71DCFD284A79C3B38668286BC97EC7A07EDE3FC1 \
-    DD8F2338BAE7501E3DD5AC78C273792F7D83545D \
-    B9AE9905FFD7803F25714661B63B535A4C206CA9 \
-    C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
-  ; do \
-    gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key" || \
-    gpg --keyserver pgp.mit.edu --recv-keys "$key" || \
-    gpg --keyserver keyserver.pgp.com --recv-keys "$key" ; \
-  done
+if [ -z $CICO_LOCAL ]; then
+    [ -f jenkins-env ] && cat jenkins-env | grep -e PASS -e USER -e GIT -e DEVSHIFT > inherit-env
+    [ -f inherit-env ] && . inherit-env
 
-yum install -y wget bzip2 git nmap-ncat psmisc \
-  && curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz" \
-  && curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt.asc" \
-  && gpg --batch --decrypt --output SHASUMS256.txt SHASUMS256.txt.asc \
-  && grep " node-v$NODE_VERSION-linux-x64.tar.xz\$" SHASUMS256.txt | sha256sum -c - \
-  && tar -xJf "node-v$NODE_VERSION-linux-x64.tar.xz" -C /usr/local --strip-components=1 \
-  && rm "node-v$NODE_VERSION-linux-x64.tar.xz" SHASUMS256.txt.asc SHASUMS256.txt \
-  && ln -s /usr/local/bin/node /usr/local/bin/nodejs \
-  && yum remove -y wget \
-  && yum clean all
+    # We need to disable selinux for now, XXX
+    /usr/sbin/setenforce 0
 
-# Build
-npm install
-echo "NPM Install Complete: $(date) $line"
-## Exec unit tests
-npm test
+    # Get all the deps in
+    yum -y install docker make git
+
+    # Get all the deps in
+    yum -y install docker make git
+    service docker start
+fi
+
+#BUILD
+docker build -t ${BUILDER_IMAGE} -f Dockerfile.build .
+
+mkdir ${TARGET_DIR}/
+docker run --detach=true --name ${BUILDER_CONT} -t -v $(pwd)/${TARGET_DIR}:/${TARGET_DIR}:Z ${BUILDER_IMAGE} /bin/tail -f /dev/null #FIXME
+
+docker exec ${BUILDER_CONT} npm install
+docker exec ${BUILDER_CONT} npm test
 
 if [ $? -ne 0 ]; then
   echo 'CICO: unit tests FAIL'
   exit 1
 fi
-
-echo "Build Complete: $(date) $line"
 
