598c2f55c70a096b0dea0186187c1bb8d12bd72c
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-04-11 08:11:21 +0200

    fix (new-ui): build status url

--- src/app/wizard/services/app-launcher-project-progress.service.ts
+++ src/app/wizard/services/app-launcher-project-progress.service.ts
@@ -15,14 +15,12 @@ export class AppLauncherProjectProgressService implements ProjectProgressService
   private END_POINT: string = '';
 
   constructor(private helperService: HelperService) {
-    if (this.helperService) {
-      this.END_POINT = this.helperService.getBackendUrl();
-      this.END_POINT = this.END_POINT.split('/api')[0];
-      if (this.END_POINT.indexOf('https') !== -1) {
-        this.END_POINT = this.END_POINT.replace('https', 'wss');
-      } else if (this.END_POINT.indexOf('http') !== -1) {
-        this.END_POINT = this.END_POINT.replace('http', 'ws');
-      }
+    this.END_POINT = this.helperService.getBackendUrl();
+    this.END_POINT = this.END_POINT.split('/api')[0];
+
+    if (this.END_POINT && (this.END_POINT.startsWith("/") || this.END_POINT.startsWith(":"))) {
+      this.END_POINT = (this.END_POINT.startsWith(":") ? location.hostname : location.host) + this.END_POINT;
+      this.END_POINT = (location.protocol === "https:" ? "wss://" : "ws://") + this.END_POINT;
     }
   }
 
