fabric8-launcher/launcher-ui-components

commit 98fc485429b283a1caef2da579364e8eb10bb519

Build on RHEL
Enable build on RHEL if TARGET=rhel.

- The build scripts will be executed twice on the same machine, once for
  CentOS and one for RHEL
- `docker login` is required to run `docker build`
- The image target is different if TARGET=rhel
- There is a separate Dockerfile for the RHEL build

cico_build_deploy.sh
@@ -5,25 +5,31 @@ REGISTRY_URI="push.registry.devshift.net"
 REGISTRY_NS="fabric8"
 REGISTRY_IMAGE="launcher-frontend"
 DOCKER_HUB_URL=${REGISTRY_NS}/${REGISTRY_IMAGE}
-REGISTRY_URL=${REGISTRY_URI}/${DOCKER_HUB_URL}
 BUILDER_IMAGE="launcher-frontend-builder"
 BUILDER_CONT="launcher-frontend-builder-container"
 DEPLOY_IMAGE="launcher-frontend-deploy"
 
-TARGET_DIR="dist"
+if [ "$TARGET" = "rhel" ]; then
+    REGISTRY_URL=${REGISTRY_URI}/osio-prod/${REGISTRY_NS}/${REGISTRY_IMAGE}
+else
+    REGISTRY_URL=${REGISTRY_URI}/${REGISTRY_NS}/${REGISTRY_IMAGE}
+fi
 
-function tag_push() {
-    TARGET_IMAGE=$1
-    USERNAME=$2
-    PASSWORD=$3
-    REGISTRY=$4
+function docker_login() {
+    local USERNAME=$1
+    local PASSWORD=$2
+    local REGISTRY=$3
 
-    docker tag ${DEPLOY_IMAGE} ${TARGET_IMAGE}
     if [ -n "${USERNAME}" ] && [ -n "${PASSWORD}" ]; then
         docker login -u ${USERNAME} -p ${PASSWORD} ${REGISTRY}
     fi
-    docker push ${TARGET_IMAGE}
+}
 
+function tag_push() {
+    local TARGET_IMAGE=$1
+
+    docker tag ${DEPLOY_IMAGE} ${TARGET_IMAGE}
+    docker push ${TARGET_IMAGE}
 }
 
 # Exit on error
@@ -39,8 +45,6 @@ if [ -z $CICO_LOCAL ]; then
     # Get all the deps in
     yum -y install docker make git
 
-    # Get all the deps in
-    yum -y install docker make git
     service docker start
 fi
 
@@ -60,14 +64,22 @@ docker exec ${BUILDER_CONT} npm run build:prod
 docker exec -u root ${BUILDER_CONT} cp -r ${TARGET_DIR}/ /
 
 #BUILD DEPLOY IMAGE
-docker build -t ${DEPLOY_IMAGE} -f Dockerfile.deploy .
+if [ "$TARGET" = "rhel" ]; then
+    # We need to be logged in for the RHEL build
+    docker_login "${DEVSHIFT_USERNAME}" "${DEVSHIFT_PASSWORD}" "${REGISTRY_URI}"
+    DOCKERFILE="Dockerfile.deploy.rhel"
+else
+    DOCKERFILE="Dockerfile.deploy"
+fi
+
+docker build -t ${DEPLOY_IMAGE} -f "${DOCKERFILE}" .
 
 #PUSH
 if [ -z $CICO_LOCAL ]; then
     TAG=$(echo $GIT_COMMIT | cut -c1-${DEVSHIFT_TAG_LEN})
 
-    tag_push "${REGISTRY_URL}:${TAG}" ${DEVSHIFT_USERNAME} ${DEVSHIFT_PASSWORD} ${REGISTRY_URI}
-    tag_push "${REGISTRY_URL}:latest" ${DEVSHIFT_USERNAME} ${DEVSHIFT_PASSWORD} ${REGISTRY_URI}
+    tag_push "${REGISTRY_URL}:${TAG}"
+    tag_push "${REGISTRY_URL}:latest"
 
     if [ -n "${GENERATOR_DOCKER_HUB_PASSWORD}" ]; then
         tag_push "${DOCKER_HUB_URL}:${TAG}" ${GENERATOR_DOCKER_HUB_USERNAME} ${GENERATOR_DOCKER_HUB_PASSWORD}

Dockerfile.deploy.rhel
@@ -1,0 +1,19 @@
+FROM prod.registry.devshift.net/osio-prod/base/openshift-nginx:latest
+LABEL maintainer "Devtools <devtools@redhat.com>"
+LABEL author "Devtools <devtools@redhat.com>"
+
+ENV LANG=en_US.utf8
+ENTRYPOINT /usr/bin/entrypoint.sh
+
+USER root
+ADD nginx.conf /etc/nginx/nginx.conf
+
+COPY scripts/entrypoint.sh /usr/bin/entrypoint.sh
+RUN chmod +x /usr/bin/entrypoint.sh
+
+RUN rm /usr/share/nginx/html/*
+
+COPY dist /usr/share/nginx/html/launch
+RUN chmod a+w /usr/share/nginx/html/launch
+
+USER 997

commit 0f0b5070a39943b23456183497c56e836e2c675f

Fixes build
This bug was introduced by:
https://github.com/fabric8-launcher/launcher-frontend/pull/315/files

cico_build_deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -x
+
 GENERATOR_DOCKER_HUB_USERNAME=openshiftioadmin
 REGISTRY_URI="push.registry.devshift.net"
 REGISTRY_NS="fabric8"
@@ -8,6 +10,8 @@ DOCKER_HUB_URL=${REGISTRY_NS}/${REGISTRY_IMAGE}
 BUILDER_IMAGE="launcher-frontend-builder"
 BUILDER_CONT="launcher-frontend-builder-container"
 DEPLOY_IMAGE="launcher-frontend-deploy"
+
+TARGET_DIR="dist"
 
 if [ "$TARGET" = "rhel" ]; then
     REGISTRY_URL=${REGISTRY_URI}/osio-prod/${REGISTRY_NS}/${REGISTRY_IMAGE}