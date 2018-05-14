14bb1fcefa9bc14faec2d75c9a3a4f8b83534968
ia3andy <adamevin@redhat.com>
2018-05-14 10:33:05 +0200

    fix: remove config load in error handler

--- src/app/shared/error.component.ts
+++ src/app/shared/error.component.ts
@@ -1,30 +1,24 @@
-import { ErrorHandler, Injectable } from "@angular/core";
+import { ErrorHandler } from "@angular/core";
 import * as Raven from 'raven-js';
-import {LaunchConfig} from "./config.component";
+import {Config} from "ngx-forge";
 
 class RavenErrorHandler extends ErrorHandler {
   handleError(err: any): void {
-    if (!err) {
-      err = new Error('Handling error without actual error, weird...');
-    }
     Raven.captureException(err.originalError || err);
     super.handleError(err);
   }
 }
 
-export function errorHandlerFactory(config: LaunchConfig) {
-  return config.load().then(() => {
-    const sentryDsn = config.get('sentry_dsn');
-    if (sentryDsn) {
-      const config = {
-        environment: process.env.ENV
-      };
-      console.info('Starting Error Handler with Sentry DSN: ' + sentryDsn);
-      Raven.config(sentryDsn, config).install();
-      return new RavenErrorHandler();
-    }
-    console.info('Starting Default Error Handler');
-    return new ErrorHandler();
-  });
+export function errorHandlerFactory(config: Config) {
+  const sentryDsn = config.get('sentry_dsn');
+  if (sentryDsn) {
+    const config = {
+      environment: process.env.ENV
+    };
+    console.info('Starting Error Handler with Sentry DSN: ' + sentryDsn);
+    Raven.config(sentryDsn, config).install();
+    return new RavenErrorHandler();
+  }
+  console.info('Starting Default Error Handler');
+  return new ErrorHandler();
 }
-
