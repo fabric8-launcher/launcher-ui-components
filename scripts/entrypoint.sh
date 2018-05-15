6f1e0342487d062fef201c448d108b7b7bc4e398
George Gastaldi <gegastaldi@gmail.com>
2018-05-14 23:47:03 -0300

    Print index.html in logs

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,9 @@ LAUNCH="/usr/share/nginx/html/launch"
 
 INDEX="${LAUNCH}/index.html"
 
+cat ${INDEX}
+
+echo -------------------------------------
 # process and export json settings from given template (escape double quotes, remove spaces and line breaks)
 export LAUNCHER_JSON_SETTINGS="$(envsubst < ${JSON_SETTINGS_TEMPLATE} | tr -d '[:space:]' | sed 's/\"/\\\"/g')"
 
@@ -12,5 +15,8 @@ echo LAUNCHER_JSON_SETTINGS=${LAUNCHER_JSON_SETTINGS}
 
 # create injected index.html with json settings
 envsubst < ${INDEX} > ${INDEX}
+echo -------------------------------------
+
+cat ${INDEX}
 
 exec /run.sh
