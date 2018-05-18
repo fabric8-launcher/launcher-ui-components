10a4474359185fd782b4a30eb7e3dc0fbe7db464
George Gastaldi <gegastaldi@gmail.com>
2018-05-18 13:51:03 -0300

    fix(sentry): Sentry DSN does not like query parameters

--- src/app/shared/error.component.ts
+++ src/app/shared/error.component.ts
@@ -1,20 +1,28 @@
 import { ErrorHandler } from "@angular/core";
+import { Config } from "ngx-forge";
 import * as Raven from 'raven-js';
-import {Config} from "ngx-forge";
 
 class RavenErrorHandler extends ErrorHandler {
   handleError(err: any): void {
-    Raven.captureException(err.originalError || err);
+    if (err) {
+      Raven.captureException(err.originalError || err);
+    }
     super.handleError(err);
   }
 }
 
 export function errorHandlerFactory(config: Config) {
-  const sentryDsn = config.get('sentry_dsn');
+  var sentryDsn = config.get('sentry_dsn');
   if (sentryDsn) {
+    const sentryURL = new URL(sentryDsn);
     const config = {
-      environment: process.env.ENV
+      environment: sentryURL.searchParams.get('environment') || process.env.ENV
     };
+    // Remove all query parameters
+    var paramIdx = sentryDsn.indexOf('?');
+    if (paramIdx > -1)  {
+      sentryDsn = sentryDsn.substring(0, paramIdx);
+    }
     console.info('Starting Error Handler with Sentry DSN: ' + sentryDsn);
     Raven.config(sentryDsn, config).install();
     return new RavenErrorHandler();
