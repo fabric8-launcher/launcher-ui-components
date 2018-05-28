836ba1161feea9070b65dec30707b93011b0e304
ia3andy <adamevin@redhat.com>
2018-05-28 12:36:24 +0200

    fix: use intro component and remove old wizard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,75 +1,30 @@
-import { NgModule }  from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { FormComponent } from "./wizard/wizard.component";
-import { IntroComponent } from "./wizard/pages/intro/intro.component";
-import { WizardComponent } from "./wizard/new-wizard.component";
-import { GettingStartedComponent } from "./wizard/pages/getting-started/getting-started.component";
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {IntroComponent} from "./wizard/pages/intro/intro.component";
+import {WizardComponent} from "./wizard/new-wizard.component";
+import {GettingStartedComponent} from "./wizard/pages/getting-started/getting-started.component";
 
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "new-wizard",
+    component: IntroComponent,
+    data: {
+      name: "intro"
+    },
     pathMatch: "full",
   },
   {
-    /* keeping this in pace in case someone bookmarked the old rotues */
-    path: "wizard",
-    children: [
-      {
-        path: "",
-        redirectTo: "/filtered-wizard/all",
-        pathMatch: "full",
-      },
-      {
-        path: ":command/:step",
-        redirectTo: "/filtered-wizard/all/:command/:step",
-        pathMatch: "full",
-      },
-      {
-        path: ":command/:step/:state",
-        redirectTo: "/filtered-wizard/all/:command/:step/:state",
-        pathMatch: "full",
-      }
-    ]
-  },
-  {
-    path: "filtered-wizard",
-    children: [
-      {
-        path: ":filters",
-        component: IntroComponent,
-        data: {
-          name: "intro"
-        }
-      },
-      {
-        path: ":filters/:command/:step",
-        component: FormComponent,
-        data: {
-          name: "step"
-        }
-      },
-      {
-        path: ":filters/:command/:step/:state",
-        component: FormComponent,
-        data: {
-          name: "step"
-        }
-      }
-    ]
-  },
-  {
-    path: 'new-wizard/:projectName',
+    path: 'wizard/:projectName',
     component: WizardComponent,
     pathMatch: 'full'
     
   },
   {
-    path: "new-wizard",
+    path: "wizard",
     component: GettingStartedComponent,
     pathMatch: 'full'
   },
-  { path: '**', redirectTo: '/new-wizard', pathMatch: 'full' }
+  { path: '**', redirectTo: '/wizard', pathMatch: 'full' }
 ];
 
 @NgModule({
