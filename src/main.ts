5320d7c68ae3e508803a46f5fc536ac006ffb19e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-06-20 07:41:54 +0200

    fix: setup static injector

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
+import { StaticInjector } from "ngx-forge";
 
 if (process.env.ENV === "production") {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(modRef => {
+  StaticInjector.setInjector(modRef.injector);
+});
