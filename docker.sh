1c8f145819290f44d57ebe745d512362efa3ecfd
Tako Schotanus <tako@ceylon-lang.org>
2018-08-14 17:39:19 +0200

    feat(docker): Added `--keycloak` option to `docker.sh`

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@
 
 DO_BUILD=1
 DO_RUN=1
+USE_KEYCLOAK=0
 
 # this is for when we run without the "--net" option.
 # Nginx doens't like it when it can't reach the launcher-backend
@@ -28,15 +29,24 @@ while [[ $# -gt 0 ]]; do
                 ;;
         --run) DO_BUILD=0
                 ;;
+        --ghuser) LAUNCHER_MISSIONCONTROL_GITHUB_USERNAME="$2"
+                shift
+                ;;
+        --ghtoken) LAUNCHER_MISSIONCONTROL_GITHUB_TOKEN="$2"
+                shift
+                ;;
+        --keycloak) USE_KEYCLOAK=1
+                ;;
         --help) echo "Usage: docker.sh [options]"
                 echo ""
                 echo "Builds and runs this project's Docker image"
                 echo ""
                 echo "Options:"
-                echo "   --build  : Only build the Docker image"
-                echo "   --run    : Only run the Docker image"
-                echo "   --net    : When run the Docker image will be attached to a private network"
-                echo "   --help   : This help"
+                echo "   --build    : Only build the Docker image"
+                echo "   --run      : Only run the Docker image"
+                echo "   --net      : When run the Docker image will be attached to a private network"
+                echo "   --keycloak : Don't use local minishift but official keycloak server"
+                echo "   --help     : This help"
                 echo ""
                 echo "For all other available options see 'docker run --help'"
                 exit
@@ -57,21 +67,36 @@ if [[ $DO_RUN -eq 1 ]]; then
     # remove any pre-existing image
     docker rm -f launcher-frontend >/dev/null 2>&1
 
-    if [[ -z "${LAUNCHER_BACKEND_URL}" ]]; then
-        echo "Missing environment variables, running with default values..."
-        # Authentication: No KeyCloak
-        unset LAUNCHER_KEYCLOAK_URL
-        unset LAUNCHER_KEYCLOAK_REALM
-        unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_CLUSTERS_FILE
-        unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_TOKEN
-        export LAUNCHER_MISSIONCONTROL_OPENSHIFT_USERNAME=developer
-        export LAUNCHER_MISSIONCONTROL_OPENSHIFT_PASSWORD=developer
+    if [[ -z "${LAUNCHER_MISSIONCONTROL_OPENSHIFT_API_URL}" && -z "${LAUNCHER_KEYCLOAK_URL}" ]]; then
+        if [[ $USE_KEYCLOAK -eq 1 ]]; then
+            echo "Missing environment variables, running with default values for Keycloak..."
+            # Authentication: Official keycloak
+            export LAUNCHER_KEYCLOAK_URL=https://sso.openshift.io/auth
+            export LAUNCHER_KEYCLOAK_REALM=rh-developers-launch
+            export LAUNCHER_MISSIONCONTROL_OPENSHIFT_CLUSTERS_FILE=$SCRIPT_DIR/clusters.yaml
+            unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_API_URL
+            unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_CONSOLE_URL
+            unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_USERNAME
+            unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_PASSWORD
+            unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_TOKEN
+            unset LAUNCHER_MISSIONCONTROL_GITHUB_USERNAME
+            unset LAUNCHER_MISSIONCONTROL_GITHUB_TOKEN
+        else
+            echo "Missing environment variables, running with default values for local minishift..."
+            # Authentication: No KeyCloak
+            unset LAUNCHER_KEYCLOAK_URL
+            unset LAUNCHER_KEYCLOAK_REALM
+            unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_CLUSTERS_FILE
+            unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_TOKEN
+            export LAUNCHER_MISSIONCONTROL_OPENSHIFT_USERNAME=developer
+            export LAUNCHER_MISSIONCONTROL_OPENSHIFT_PASSWORD=developer
+        fi
         # For launcher-frontend
         if [[ "$NETWORK" -eq "default" ]]; then
             export LAUNCHER_BACKEND_URL="http://127.0.0.1:8080/api"
         fi
     fi
-    
+
 	# run it
 	echo "Running image..."
 	docker run \
