6238b8d9e42bd1bf229233486ebf0a2e28b67c0f
ia3andy <adamevin@redhat.com>
2019-02-22 11:31:34 +0100

    fix: use global for styling

--- packages/launcher-component/src/core/item-picker/item-picker.tsx
+++ packages/launcher-component/src/core/item-picker/item-picker.tsx
@@ -23,7 +23,7 @@ function ListItem(props: ListItemProps) {
     onSelect(props.id);
   };
   return (
-    <Card onClick={doOnSelect} className={classNames(style.item, { [style.selected]: selected })}>
+    <Card onClick={doOnSelect} className={classNames({selected})}>
       <CardHeader className={style.header}>
         <Radio
           aria-label={`Choose ${props.id} as ${props.group}`}
@@ -35,7 +35,7 @@ function ListItem(props: ListItemProps) {
         />
         <Title size="lg">{props.name}</Title>
       </CardHeader>
-      <CardBody className={style.body}>
+      <CardBody>
         <img src={props.icon}/>
         <p>
           {props.description}
@@ -54,7 +54,7 @@ function ItemPicker(props: ItemPickerProps) {
   const items = props.items || [];
 
   return (
-    <div className={style.module}>
+    <div className={style.itemPicker}>
       <Gallery gutter="md">
         {
           items.map((viewItem, i) => (
