8ffe2750c95afd33acca121284aac920cdbd6ca0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-02-27 14:28:20 +0100

    fix: remove the build folder

--- packages/launcher-client/build/types/client/helpers/locations.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Locations {
-    static joinPath(...parts: string[]): string;
-    static joinWithSlash(start: string, end?: string): string;
-    static createWebsocketUrl(url: string): string;
-}
--- packages/launcher-client/build/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './client/launcher.client.factory';
-export * from './client/launcher.client';
-export * from './client/types';
-export * from './client/helpers/launchers';
-export * from './client/helpers/preconditions';
