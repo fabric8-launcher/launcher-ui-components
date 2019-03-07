2e9a8c75c120cd3f9fea1f72c5f9fefab2f81e6f
ia3andy <adamevin@redhat.com>
2019-03-07 15:13:40 +0100

    fix: div cannot be added inside p

--- packages/launcher-component/src/core/hub-n-spoke/overview-complete.tsx
+++ packages/launcher-component/src/core/hub-n-spoke/overview-complete.tsx
@@ -1,13 +1,15 @@
-import { EmptyState, EmptyStateBody, Title } from '@patternfly/react-core';
+import { Title } from '@patternfly/react-core';
 import { CheckCircleIcon } from '@patternfly/react-icons';
 import * as React from 'react';
 import style from './hub-n-spoke.module.scss';
 
 export function OverviewComplete(props: { title: string; children?: React.ReactNode }) {
   return (
-    <EmptyState>
-      <Title size="lg"><CheckCircleIcon className={style.checkComplete}/>{props.title}</Title>
-      {props.children && (<EmptyStateBody>{props.children}</EmptyStateBody>)}
-    </EmptyState>
+    <div className={style.overviewComplete}>
+      <Title size="lg" className={style.overviewCompleteTitle}>
+        <CheckCircleIcon className={style.checkComplete}/>{props.title}
+      </Title>
+      {props.children && (<div className={style.overviewCompleteBody}>{props.children}</div>)}
+    </div>
   );
 }
