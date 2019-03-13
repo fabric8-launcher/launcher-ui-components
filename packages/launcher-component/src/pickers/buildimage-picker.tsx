83d1e277d393921bb3d5158304ea3fbbb1183c31
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-03-13 14:02:41 +0100

    fix: changed build image select into advanced sectionfixing: #984 (#997)

--- packages/launcher-component/src/pickers/buildimage-picker.tsx
+++ packages/launcher-component/src/pickers/buildimage-picker.tsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
-import { Form, FormGroup, FormSelect, FormSelectOption } from '@patternfly/react-core';
+import { DataListItem, DataListCell, Title, DataList, Radio, Button, Alert } from '@patternfly/react-core';
 import { AnalyzeResult } from 'launcher-client';
 import { InputProps } from '../core/types';
 import { Loader } from '../core/data-loader/data-loader';
+import { PlusCircleIcon } from '@patternfly/react-icons';
+import { SpecialValue } from '../core/stuff';
 
 export const defaultBuidImagePickerValue = {};
 
 export interface BuildImageValue {
   imageName?: string;
+  advanced?: boolean;
 }
 
 interface BuildImageProps extends InputProps<BuildImageValue> {
@@ -16,37 +19,57 @@ interface BuildImageProps extends InputProps<BuildImageValue> {
 
 export function BuildImagePicker(props: BuildImageProps) {
   if (!props.value.imageName) {
-    props.onChange({imageName: props.result.image});
+    props.onChange({ imageName: props.result.image });
     return (<Loader />);
   }
-  const imageName = props.value.imageName;
   return (
     <Fragment>
-      <p>Analyzed your code, this is the build image we suggest: <i>"{imageName}"</i></p>
-      <p>But we could be wrong, you can change the image here:</p>
-      <Form>
-        <FormGroup
-          label="Build image"
-          isRequired
-          fieldId="imageName"
-        >
-          <FormSelect
-            id="imageName"
-            value={imageName}
-            onChange={value => props.onChange({imageName: value})}
-            aria-label="Select build image"
-          >
-            {props.result.builderImages.map((image, index) => (
-              <FormSelectOption
-                key={index}
-                value={image.id}
-                label={image.name}
-              />
-            ))
+      <p>
+        For your codebase, our runtime detection algorithm suggest this image: <SpecialValue>{props.result.image}</SpecialValue>
+      </p>
+      <Button
+        // @ts-ignore
+        component="a"
+        variant="link"
+        onClick={() => props.onChange({ ...props.value, advanced: true })}
+      >
+        <PlusCircleIcon /> Advanced settings
+      </Button>
+      {props.value.advanced &&
+        <Fragment>
+          <Alert variant="warning" title="Picking the wrong image may result in an failed deployment!" style={{ margin: '20px' }}/>
+          <DataList aria-label="select-buildImage">
+            {props.result.builderImages.map((image, index) => {
+              const isSelected = props.value.imageName === image.id;
+              const onChangeSelected = () => {
+                props.onChange({ ...props.value, imageName: image.id });
+              };
+
+              return (
+                <DataListItem aria-labelledby={image.name} isExpanded={false} key={index}>
+                  <DataListCell width={1} style={{ flex: 'none' }}>
+                    <Radio
+                      aria-label={`Choose ${image.name} as mission`}
+                      value={image.id}
+                      checked={isSelected}
+                      onChange={onChangeSelected}
+                      name="imageName"
+                      id={`radio-choose-${image.id}-as-image`}
+                    />
+                  </DataListCell>
+                  <DataListCell width={1} onClick={onChangeSelected} style={{ cursor: 'pointer' }}>
+                    <Title size="lg">{image.name}</Title>
+                  </DataListCell>
+                  <DataListCell width={2} onClick={onChangeSelected} style={{ cursor: 'pointer' }}>
+                    {image.id}
+                  </DataListCell>
+                </DataListItem>
+              );
+            })
             }
-          </FormSelect>
-        </FormGroup>
-      </Form>
+          </DataList>
+        </Fragment>
+      }
     </Fragment>
   );
 }
