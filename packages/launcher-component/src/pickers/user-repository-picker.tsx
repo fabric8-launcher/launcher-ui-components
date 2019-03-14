acfd51f32efc4aa600d54cdd78dbfeee09428fd4
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-03-14 15:22:03 +0100

    fix: removed import and improved stylesfixes: #1005

--- packages/launcher-component/src/pickers/user-repository-picker.tsx
+++ packages/launcher-component/src/pickers/user-repository-picker.tsx
@@ -16,30 +16,32 @@ interface UserRepositoryPickerProps extends InputProps<UserRepositoryPickerValue
   gitInfo: GitInfo;
 }
 
-export function valueToPath(value: UserRepositoryPickerValue) {
+export function valueToPath(value: UserRepositoryPickerValue, login?: string) {
   if (value.org) {
     return `${value.org}/${value.name}`;
+  }
+  if (login) {
+    return `${login}/${value.name}`;
   }
   return value.name || '';
 }
 
-const isExistingRepository = (repositories: string[], value: UserRepositoryPickerValue): boolean => {
-  return repositories.indexOf(valueToPath(value)) !== -1;
+const isExistingRepository = (repositories: string[], props: UserRepositoryPickerProps): boolean => {
+  return repositories.indexOf(valueToPath(props.value, props.gitInfo.login)) !== -1;
 };
 
 export const UserRepositoryPicker: Picker<UserRepositoryPickerProps, UserRepositoryPickerValue> = {
   checkCompletion: value => (!value.org || REPOSITORY_VALUE_REGEXP.test(value.org)) && REPOSITORY_VALUE_REGEXP.test(value.name || ''),
   Element: props => {
     const name = props.value.name || '';
-    const org = props.value.org || props.gitInfo.organizations[0];
-    const helperRepoInvalid = isExistingRepository(props.gitInfo.repositories, props.value) ?
-      `Repository already exists ${valueToPath(props.value)}` : 'Invalid repository name';
+    const helperRepoInvalid = isExistingRepository(props.gitInfo.repositories, props) ?
+      `Repository already exists ${valueToPath(props.value, props.gitInfo.login)}` : 'Invalid repository name';
     const isRepoValid = UserRepositoryPicker.checkCompletion(props.value)
-      && !isExistingRepository(props.gitInfo.repositories, props.value);
+      && !isExistingRepository(props.gitInfo.repositories, props);
     return (
       <Grid>
-        <GridItem span={4} style={{margin: 'auto'}}>
-          <img src={props.gitInfo.avatarUrl} className={style.avatar}/>
+        <GridItem span={4} className={style.avatar}>
+          <img src={props.gitInfo.avatarUrl} />
           <p><b>{props.gitInfo.login}</b></p>
         </GridItem>
         <GridItem span={8}>
@@ -52,12 +54,12 @@ export const UserRepositoryPicker: Picker<UserRepositoryPickerProps, UserReposit
             >
               <FormSelect
                 id="ghOrg"
-                value={org}
-                onChange={value => props.onChange({...props.value, org: value})}
+                value={props.value.org}
+                onChange={value => value ? props.onChange({ ...props.value, org: value }) : props.onChange({ name: props.value.name })}
                 aria-label="Select organization"
               >
                 <FormSelectOption
-                  value={props.gitInfo.login}
+                  value={undefined}
                   label={props.gitInfo.login}
                 />
                 {props.gitInfo.organizations.map((o, index) => (
@@ -83,7 +85,7 @@ export const UserRepositoryPicker: Picker<UserRepositoryPickerProps, UserReposit
                 name="ghRepo-name"
                 placeholder="Select Repository"
                 aria-describedby="Select Repository"
-                onChange={value => props.onChange({...props.value, name: value})}
+                onChange={value => props.onChange({ ...props.value, name: value })}
                 value={name}
                 pattern={REPOSITORY_VALUE_REGEXP.source}
                 title="Valid repository name"
