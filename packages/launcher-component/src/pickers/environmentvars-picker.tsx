8cac194b7c9ed8b7e66115e734a984140d3cfb89
ia3andy <adamevin@redhat.com>
2019-03-21 11:06:33 +0100

    fix: use array instead of map (more adapted) and avoid mutation

--- packages/launcher-component/src/pickers/environmentvars-picker.tsx
+++ packages/launcher-component/src/pickers/environmentvars-picker.tsx
@@ -7,75 +7,79 @@ import { InputProps, Picker } from '../core/types';
 const VALID_ENV_KEY_REGEXP = new RegExp('^$|^[-._a-zA-Z][-._a-zA-Z0-9]*$');
 
 export interface EnvironmentVarsPickerValue {
-  envVars: Map<string, string>;
+  envVars: Array<{ key: string; value: string }>;
 }
 
 interface EnvironmentVarsPickerProps extends InputProps<EnvironmentVarsPickerValue> {
 }
 
+const NEW_ENTRY = {key: '', value: ''};
+
 export const EnvironmentVarsPicker: Picker<EnvironmentVarsPickerProps, EnvironmentVarsPickerValue> = {
   checkCompletion: value => !!value.envVars
-    && Array.from(value.envVars.keys()).map(key => VALID_ENV_KEY_REGEXP.test(key)).reduce((a, b) => a && b),
+    && value.envVars.map(entry => VALID_ENV_KEY_REGEXP.test(entry.key)).reduce((a, b) => a && b),
   Element: props => {
-    const values = props.value.envVars || new Map([['', '']]);
+    const entries = props.value.envVars || [NEW_ENTRY];
     const isValid: (value: string) => boolean = value => VALID_ENV_KEY_REGEXP.test(value || '');
-    let index = 0;
     return (
       <Fragment>
-        {Array.from(values.keys()).map(key => {
-          index++;
-          return (
-            <Split key={'split' + index} gutter="sm">
-              <SplitItem isMain key={'split-name' + index}>
-                <TextInput
-                  isRequired
-                  type="text"
-                  key={'name' + index}
-                  id={'env-var-name' + index}
-                  name="env-var-name"
-                  placeholder="Type the environment variable name"
-                  pattern="[-._a-zA-Z][-._a-zA-Z0-9]*"
-                  onChange={value => {
-                    values.set(value, values.get(key) || '');
-                    values.delete(key);
-                    props.onChange({ envVars: values });
-                  }}
-                  isValid={isValid(key)}
-                  value={key}
-                />
-              </SplitItem>
-              <SplitItem isMain key={'split-value' + key}>
-                <TextInput
-                  isRequired
-                  type="text"
-                  key={'value' + index}
-                  id={'env-var-value' + index}
-                  name="env-var-value"
-                  placeholder="Type the environment variable value"
-                  onChange={value => {
-                    values.set(key, value);
-                    props.onChange({ envVars: values });
-                  }}
-                  value={values.get(key)}
-                />
-              </SplitItem>
-              <SplitItem isMain={false} key={'split-button' + index}>
-                <Button
-                  onClick={() => {
-                    values.delete(key);
-                    if (values.size === 0) {
-                      values.set('', '');
-                    }
-                    props.onChange({ envVars: values });
-                  }}
-                >
-                  <TrashIcon />
-                </Button>
-              </SplitItem>
-            </Split>);
-        }
+        {entries.map((entry, index) => {
+            return (
+              <Split key={'split' + index} gutter="sm">
+                <SplitItem isMain key={'split-name' + index}>
+                  <TextInput
+                    isRequired
+                    type="text"
+                    key={'name' + index}
+                    id={'env-var-name' + index}
+                    name="env-var-name"
+                    placeholder="Type the environment variable name"
+                    pattern="[-._a-zA-Z][-._a-zA-Z0-9]*"
+                    onChange={newKey => {
+                      const newEntries = entries.slice();
+                      newEntries[index] =  {key: newKey, value: entry.value};
+                      props.onChange({envVars: newEntries});
+                    }}
+                    isValid={isValid(entry.key)}
+                    value={entry.key}
+                  />
+                </SplitItem>
+                <SplitItem isMain key={'split-value' + index}>
+                  <TextInput
+                    isRequired
+                    type="text"
+                    key={'value' + index}
+                    id={'env-var-value' + index}
+                    name="env-var-value"
+                    placeholder="Type the environment variable value"
+                    onChange={newValue => {
+                      const newEntries = entries.slice();
+                      newEntries[index] =  {key: entry.key, value:newValue};
+                      props.onChange({envVars: newEntries});
+                    }}
+                    value={entry.value}
+                  />
+                </SplitItem>
+                <SplitItem isMain={false} key={'split-button' + index}>
+                  <Button
+                    onClick={() => {
+                      let newEntries;
+                      if (entries.length <= 1) {
+                        newEntries = [NEW_ENTRY];
+                      } else {
+                        newEntries = entries.slice();
+                        newEntries.splice(index, 1);
+                      }
+                      props.onChange({envVars: newEntries});
+                    }}
+                  >
+                    <TrashIcon/>
+                  </Button>
+                </SplitItem>
+              </Split>);
+          }
         )}
-        <Button onClick={() => props.onChange({ envVars: values.set('', '') })}><PlusCircleIcon /></Button>
+        <Button onClick={() => props.onChange({envVars: [...entries, NEW_ENTRY]})}><PlusCircleIcon/></Button>
       </Fragment>
     );
   }
