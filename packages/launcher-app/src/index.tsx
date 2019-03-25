7dbcd30a294d06826964f5e90be67a289f1dec83
ia3andy <adamevin@redhat.com>
2019-03-25 11:59:16 +0100

    feat: introduce sentry error tracking
    Closes #1001

--- packages/launcher-app/src/index.tsx
+++ packages/launcher-app/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { LauncherApp } from './launcher-app';
 import * as serviceWorker from './service-worker';
+import { SentryBoundary } from './sentry-boundary';
 
-ReactDOM.render(<LauncherApp />, document.getElementById('root'));
+ReactDOM.render(<SentryBoundary><LauncherApp /></SentryBoundary>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
