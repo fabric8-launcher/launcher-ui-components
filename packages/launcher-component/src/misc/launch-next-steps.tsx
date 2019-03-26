a971c9373e97a2f6c46721fbb0b2bcf12943b0cb
Andy Damevin <adamevin@redhat.com>
2019-03-26 15:38:45 +0100

    fix: make consoleUrl and repositoryUrl optional (#1133)

--- packages/launcher-component/src/misc/launch-next-steps.tsx
+++ packages/launcher-component/src/misc/launch-next-steps.tsx
@@ -12,8 +12,8 @@ interface LaunchNextStepsProps {
 
 export function LaunchNextSteps(props: LaunchNextStepsProps) {
   const links = props.links || {};
-  const repositoryLink = links['GITHUB_CREATE'] || 'https://github.com/fabric8-launcher/launcher-creator-frontend';
-  const deploymentLink = links['OPENSHIFT_CREATE'] || 'https://manage.openshift.com/';
+  const repositoryLink = links['GITHUB_CREATE'];
+  const consoleLink = links['OPENSHIFT_CREATE'];
   const welcomeLink = links['welcome'];
   return (
     <FixedModal
@@ -30,30 +30,29 @@ export function LaunchNextSteps(props: LaunchNextStepsProps) {
       <TextContent>
         <Text component={TextVariants.h3}>Follow your application delivery</Text>
         <Text component={TextVariants.p}>You can follow your application deployment in your OpenShift Console</Text>
-        <ExternalLink href={deploymentLink}>
-          <ClusterIcon /> OpenShift Console
-        </ExternalLink>
-
+        {consoleLink && (<ExternalLink href={consoleLink}>
+          <ClusterIcon/> OpenShift Console
+        </ExternalLink>)}
         {
           welcomeLink &&
           <Fragment>
-            <Text component={TextVariants.h3}>As soon as deployment is done, check out your new application capabilities</Text>
-            <Text component={TextVariants.p}>
-              We prepared a set of examples to let you directly start playing with your new application.<br />
-              Those examples are there to get you started,<br />
-              soon it will be time for you to remove them and start developing your awesome application.</Text>
-            <ExternalLink href={welcomeLink}>
-              <GiftIcon /> Check out your new Application
-            </ExternalLink>
+              <Text component={TextVariants.h3}>As soon as deployment is done, check out your new application capabilities</Text>
+              <Text component={TextVariants.p}>
+                  We prepared a set of examples to let you directly start playing with your new application.<br/>
+                  Those examples are there to get you started,<br/>
+                  soon it will be time for you to remove them and start developing your awesome application.</Text>
+              <ExternalLink href={welcomeLink}>
+                  <GiftIcon/> Check out your new Application
+              </ExternalLink>
           </Fragment>
         }
         <Text component={TextVariants.h3}>Update your application using Continuous Delivery</Text>
         <Text component={TextVariants.p}>We set up your application codebase in the GitHub repository you requested</Text>
         <Text component={TextVariants.p}>Your application is automatically configured
           to build and deploy on OpenShift with new commits.</Text>
-        <ExternalLink href={repositoryLink}>
-          <CodeIcon /> Clone your new codebase
-        </ExternalLink>
+        {repositoryLink && (<ExternalLink href={repositoryLink}>
+          <CodeIcon/> Clone your new codebase
+        </ExternalLink>)}
       </TextContent>
     </FixedModal>
   );
