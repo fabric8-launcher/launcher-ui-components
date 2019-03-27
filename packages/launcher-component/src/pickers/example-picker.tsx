40d04741a2d17f05e1558dc0212d466d5119fdee
Andy Damevin <adamevin@redhat.com>
2019-03-27 11:43:13 +0100

    feat: prevent launch on istio example (#1142)
    * feat: add downloadOnly flag for example and clusterType to ClusterPicker value
    
    * fix: filtering of missions and runtimes
    
    * fix: added 'downloadOnly' flag based on === istio
    
    * fix: add multiple choice storybook and sort clusters

--- packages/launcher-component/src/pickers/example-picker.tsx
+++ packages/launcher-component/src/pickers/example-picker.tsx
@@ -12,7 +12,7 @@ import {
   Split,
   SplitItem
 } from '@patternfly/react-core';
-import { Catalog, filter, ExampleRuntime, ExampleMission } from 'launcher-client';
+import { Catalog, ExampleRuntime, ExampleMission } from 'launcher-client';
 import { InputProps, Picker } from '../core/types';
 import _ from 'lodash';
 
@@ -21,6 +21,7 @@ export interface ExamplePickerValue {
   missionId?: string;
   runtimeId?: string;
   versionId?: string;
+  downloadOnly?: boolean;
 }
 
 interface ExamplePickerProps extends InputProps<ExamplePickerValue> {
@@ -31,18 +32,14 @@ interface ExamplePickerProps extends InputProps<ExamplePickerValue> {
 export const ExamplePicker: Picker<ExamplePickerProps, ExamplePickerValue> = {
   checkCompletion: value => !!value.missionId && !!value.runtimeId && !!value.versionId,
   Element: props => {
-    const query = { runtime: { id: '', name: '', version: { id: '', name: '' } } };
-    const runtimesMap = _.keyBy(filter(query, props.catalog) as ExampleRuntime[], 'id');
+    const runtimesMap = _.keyBy(props.catalog.runtimes, 'id');
     const missions = !props.value.missions ? props.missions : props.value.missions;
     const filterCatalog = (runtime) => {
+      const filteredMissions = _.cloneDeep(props.missions);
+      filteredMissions!.map(m => m.runtime = (m.runtime as ExampleRuntime[]).filter(r => r.id === runtime));
       props.onChange({
         runtimeId: runtime,
-        missions: (filter({
-          mission: {
-            id: '', name, description: '', runtime:
-              { id: runtime, name, version: { name, id: '' } }
-          }
-        }, props.catalog) as ExampleMission[]).filter(m => !!m.runtime)
+        missions: filteredMissions!.filter(m => (m.runtime as ExampleRuntime[]).find(r => !!r))
       });
     };
 
@@ -56,7 +53,7 @@ export const ExamplePicker: Picker<ExamplePickerProps, ExamplePickerValue> = {
             <Button variant="tertiary" onClick={() => props.onChange({ ...props.value, missions: undefined, runtimeId: '' })}>Any</Button>
             {_.map(runtimesMap).map((runtime, index) => (
               <Button
-                style={{marginLeft: '5px'}}
+                style={{ marginLeft: '5px' }}
                 variant="tertiary"
                 key={index}
                 onClick={() => filterCatalog(runtime.id)}
@@ -67,7 +64,7 @@ export const ExamplePicker: Picker<ExamplePickerProps, ExamplePickerValue> = {
             ))}
           </SplitItem>
         </Split>
-        <DataList aria-label="select-mission" style={{marginTop: '20px'}}>
+        <DataList aria-label="select-mission" style={{ marginTop: '20px' }}>
           {
             missions!.map((mission, i) => {
               const isSelected = props.value.missionId === mission.id;
@@ -119,7 +116,12 @@ export const ExamplePicker: Picker<ExamplePickerProps, ExamplePickerValue> = {
                       <FormSelect
                         id={mission.id + 'version-select'}
                         value={props.value.versionId}
-                        onChange={value => props.onChange({ ...props.value, versionId: value })}
+                        onChange={value => props.onChange(
+                          {
+                            ...props.value,
+                            versionId: value,
+                            downloadOnly: mission.metadata.istio || false
+                          })}
                         aria-label="Select Version"
                       >
                         <FormSelectOption
@@ -127,13 +129,14 @@ export const ExamplePicker: Picker<ExamplePickerProps, ExamplePickerValue> = {
                           value=""
                           label="Select a Version"
                         />
-                        {(runtimesMap[props.value.runtimeId] as any).version.map((version, index) => (
-                          <FormSelectOption
-                            key={index}
-                            value={version.id}
-                            label={version.name}
-                          />
-                        ))
+                        {(mission.runtime! as any).find(r => r.id === props.value.runtimeId) &&
+                          (mission.runtime! as any).find(r => r.id === props.value.runtimeId).versions.map((version, index) => (
+                            <FormSelectOption
+                              key={index}
+                              value={version.id}
+                              label={version.name}
+                            />
+                          ))
                         }
                       </FormSelect>
                     }
