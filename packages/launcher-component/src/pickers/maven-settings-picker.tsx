8a78b7e82d50d9f09e6d7530ef300af72a0b6bf8
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-04-23 13:03:17 +0200

    fix: introduced base TextInput component that only tests required if the field is dirty

--- packages/launcher-component/src/pickers/maven-settings-picker.tsx
+++ packages/launcher-component/src/pickers/maven-settings-picker.tsx
@@ -1,6 +1,7 @@
-import { Form, FormGroup, TextInput } from '@patternfly/react-core';
+import { Form } from '@patternfly/react-core';
 import React from 'react';
 import { InputProps, Picker } from '../core/types';
+import { LaunchTextInput } from '../core/text-input/text-input';
 
 export interface MavenSettingsPickerValue {
   groupId: string;
@@ -11,7 +12,7 @@ export interface MavenSettingsPickerValue {
 interface MavenSettingsPickerProps extends InputProps<MavenSettingsPickerValue> {
 }
 
-const VALUE_REGEXP = /^$|^[a-z][a-z0-9-\.]{3,63}$/;
+const VALUE_REGEXP = /^[a-z][a-z0-9-\.]{3,63}$/;
 
 export const MavenSettingsPicker: Picker<MavenSettingsPickerProps, MavenSettingsPickerValue> = {
   checkCompletion: value => !!value.groupId && VALUE_REGEXP.test(value.groupId)
@@ -20,62 +21,44 @@ export const MavenSettingsPicker: Picker<MavenSettingsPickerProps, MavenSettings
     const isValid = (value: string) => VALUE_REGEXP.test(value || '');
     return (
       <Form>
-        <FormGroup
+        <LaunchTextInput
           label="GroupId"
           isRequired
-          fieldId="groupId"
           helperTextInvalid="Please provide a valid groupId"
+          type="text"
+          id="groupId"
+          name="groupId"
+          aria-label="Maven groupId name"
+          value={props.value.groupId}
+          onChange={value => props.onChange({ ...props.value, groupId: value })}
+          pattern={VALUE_REGEXP.source}
           isValid={isValid(props.value.groupId)}
-        >
-          <TextInput
-            isRequired
-            type="text"
-            id="groupId"
-            name="groupId"
-            aria-label="Maven groupId name"
-            value={props.value.groupId}
-            onChange={value => props.onChange({ ...props.value, groupId: value })}
-            pattern={VALUE_REGEXP.source}
-            isValid={isValid(props.value.groupId)}
-          />
-        </FormGroup>
-        <FormGroup
+        />
+        <LaunchTextInput
           label="ArtifactId"
           isRequired
-          fieldId="artifactId"
           helperTextInvalid="Please provide a valid artifactId"
+          type="text"
+          id="artifactId"
+          name="artifactId"
+          aria-label="Maven artifactId name"
+          value={props.value.artifactId}
+          onChange={value => props.onChange({ ...props.value, artifactId: value })}
+          pattern={VALUE_REGEXP.source}
           isValid={isValid(props.value.artifactId)}
-        >
-          <TextInput
-            isRequired
-            type="text"
-            id="artifactId"
-            name="artifactId"
-            aria-label="Maven artifactId name"
-            value={props.value.artifactId}
-            onChange={value => props.onChange({ ...props.value, artifactId: value })}
-            pattern={VALUE_REGEXP.source}
-            isValid={isValid(props.value.artifactId)}
-          />
-        </FormGroup>
-        <FormGroup
+        />
+        <LaunchTextInput
           label="Version"
-          isRequired
-          fieldId="version"
           helperTextInvalid="Please provide a version number"
+          isRequired
+          type="text"
+          id="version"
+          name="version"
+          aria-label="Maven version number"
+          value={props.value.version}
+          onChange={value => props.onChange({ ...props.value, version: value })}
           isValid={!!props.value.version}
-        >
-          <TextInput
-            isRequired
-            type="text"
-            id="version"
-            name="version"
-            aria-label="Maven version number"
-            value={props.value.version}
-            onChange={value => props.onChange({ ...props.value, version: value })}
-            isValid={!!props.value.version}
-          />
-        </FormGroup>
+        />
       </Form>
     );
   }
--- packages/launcher-component/src/pickers/nodejs-settings-picker.tsx
+++ packages/launcher-component/src/pickers/nodejs-settings-picker.tsx
@@ -1,6 +1,7 @@
-import { Form, FormGroup, TextInput } from '@patternfly/react-core';
+import { Form } from '@patternfly/react-core';
 import React from 'react';
 import { InputProps, Picker } from '../core/types';
+import { LaunchTextInput } from '../core/text-input/text-input';
 
 export interface NodeJSSettingsPickerValue {
   name: string;
@@ -11,8 +12,8 @@ interface NodeJSSettingsPickerProps extends InputProps<NodeJSSettingsPickerValue
 }
 
 // @ts-ignore
-const VERSION_REGEXP = /^$|^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/;
-const NAME_REGEXP = /^$|^(?=.{1,214}$)(?:@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/;
+const VERSION_REGEXP = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/;
+const NAME_REGEXP = /^(?=.{1,214}$)(?:@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/;
 
 export const NodeJSSettingsPicker: Picker<NodeJSSettingsPickerProps, NodeJSSettingsPickerValue> = {
   checkCompletion: value => !!value.name && NAME_REGEXP.test(value.name)
@@ -20,44 +21,32 @@ export const NodeJSSettingsPicker: Picker<NodeJSSettingsPickerProps, NodeJSSetti
   Element: props => {
     return (
       <Form>
-        <FormGroup
+        <LaunchTextInput
           label="Name"
           isRequired
-          fieldId="name"
           helperTextInvalid="Please provide a valid name"
+          type="text"
+          id="name"
+          name="name"
+          aria-label="Nodejs package name"
+          value={props.value.name}
+          onChange={value => props.onChange({ ...props.value, name: value })}
+          pattern={NAME_REGEXP.source}
           isValid={NAME_REGEXP.test(props.value.name || '')}
-        >
-          <TextInput
-            isRequired
-            type="text"
-            id="name"
-            name="name"
-            aria-label="Nodejs package name"
-            value={props.value.name}
-            onChange={value => props.onChange({ ...props.value, name: value })}
-            pattern={NAME_REGEXP.source}
-            isValid={NAME_REGEXP.test(props.value.name || '')}
-          />
-        </FormGroup>
-        <FormGroup
+        />
+        <LaunchTextInput
           label="Version"
           isRequired
-          fieldId="version"
           helperTextInvalid="Please provide a valid version number"
+          type="text"
+          id="version"
+          name="version"
+          aria-label="Nodejs version"
+          value={props.value.version}
+          onChange={value => props.onChange({ ...props.value, version: value })}
           isValid={VERSION_REGEXP.test(props.value.version || '')}
-        >
-          <TextInput
-            isRequired
-            type="text"
-            id="version"
-            name="version"
-            aria-label="Nodejs version"
-            value={props.value.version}
-            onChange={value => props.onChange({ ...props.value, version: value })}
-            isValid={VERSION_REGEXP.test(props.value.version || '')}
-          />
-        </FormGroup>
-      </Form>
+        />
+      </Form >
     );
   }
 };
