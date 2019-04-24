43437ea2211140feb8e2dc1936922280503eb154
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-04-24 17:13:48 +0200

    fix: use patternfly icon instead of css

--- packages/launcher-component/src/core/inline-text-input/inline-text-input.tsx
+++ packages/launcher-component/src/core/inline-text-input/inline-text-input.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, TextInput, TextVariants, TextInputProps } from '@patternfly/react-core';
 
 import style from './inline-text-input.module.scss';
+import { EditAltIcon } from '@patternfly/react-icons';
 
 interface InlineTextInputProps extends TextInputProps {
   title: string;
 }
 
 export function InlineTextInput(props: InlineTextInputProps) {
+  const [hint, setHint] = useState(false);
   return (
     <div className={style.title}>
       <Text component={TextVariants.h1}>{props.title}</Text>
-      <div className={style.input}>
-        <TextInput {...props} type="text"/>
-      </div>
+      <TextInput {...props} type="text" onMouseEnter={() => setHint(!hint)} onMouseLeave={() => setHint(!hint)} />
+      {hint && <EditAltIcon />}
     </div>
   );
 }
