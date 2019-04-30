7010cc207b23d6b433cc7e1db5e6d4734f6eefb8
ia3andy <adamevin@redhat.com>
2019-04-30 15:58:12 +0200

    fix: use valid openshiftOAuthClientId

--- packages/launcher-app/src/app/config.ts
+++ packages/launcher-app/src/app/config.ts
@@ -41,7 +41,7 @@ function getAuthConfig(authMode: string): KeycloakConfig | OpenshiftConfig | und
     case 'oauth-openshift':
       return {
         openshift: {
-          clientId: requireEnv(process.env.REACT_APP_OAUTH_OPENSHIFT_CLIENT_ID, 'openshiftClientId'),
+          clientId: requireEnv(process.env.REACT_APP_OAUTH_OPENSHIFT_CLIENT_ID, 'openshiftOAuthClientId'),
           url: requireEnv(process.env.REACT_APP_OAUTH_OPENSHIFT_URL, 'openshiftOAuthUrl'),
           validateTokenUri: `${requireEnv(process.env.REACT_APP_LAUNCHER_API_URL, 'launcherApiUrl')}/services/openshift/user`,
         },
