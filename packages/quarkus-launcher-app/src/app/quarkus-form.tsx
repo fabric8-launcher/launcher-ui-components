35a51f25974a9a31488c7e7d0cf5e28d07b20481
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-05-27 14:24:31 +0200

    fixed whitespace

--- packages/quarkus-launcher-app/src/app/quarkus-form.tsx
+++ packages/quarkus-launcher-app/src/app/quarkus-form.tsx
@@ -6,40 +6,40 @@ interface QuarkusFormProps { }
 const validator = () => true;
 
 interface Project {
-    metadata: {
-        groupId: string;
-        artifactId: string;
-        version: string;
-        name?: string;
-        description?: string;
-        packageName?: string;
-    }
-    dependencies: string[];
+  metadata: {
+    groupId: string;
+    artifactId: string;
+    version: string;
+    name?: string;
+    description?: string;
+    packageName?: string;
+  }
+  dependencies: string[];
 }
 
 export function QuarkusForm(props: QuarkusFormProps) {
-    const [project, setProject] = React.useState<Project>({
-        metadata: {
-            groupId: 'com.example',
-            artifactId: 'quarkus-project',
-            version: 'latest',
-            name: 'quarkus-project',
-            description: 'My project with Quarkus',
-            packageName: 'com.example.quarkus-project',
-        },
-        dependencies: [],
-    });
+  const [project, setProject] = React.useState<Project>({
+    metadata: {
+      groupId: 'com.example',
+      artifactId: 'quarkus-project',
+      version: 'latest',
+      name: 'quarkus-project',
+      description: 'My project with Quarkus',
+      packageName: 'com.example.quarkus-project',
+    },
+    dependencies: [],
+  });
 
 
-    const setMetadata = (metadata: any) => setProject((prev) => ({ ...prev, metadata }));
-    const setDependencies = (val: { dependencies: string[] }) => setProject((prev) => ({ ...prev, dependencies: val.dependencies }));
-    return (
-        <div className="quarkus-form-container">
-            <MavenSettingsPicker.Element value={project.metadata} onChange={setMetadata} />
-            <Separator />
-            <EnumLoader name="quarkus-extensions">
-                {extensions => (<DependenciesPicker.Element items={extensions as DependencyItem[]} value={{ dependencies: project.dependencies }} onChange={setDependencies} />)}
-            </EnumLoader>
-        </div>
-    );
+  const setMetadata = (metadata: any) => setProject((prev) => ({ ...prev, metadata }));
+  const setDependencies = (val: { dependencies: string[] }) => setProject((prev) => ({ ...prev, dependencies: val.dependencies }));
+  return (
+    <div className="quarkus-form-container">
+      <MavenSettingsPicker.Element value={project.metadata} onChange={setMetadata} />
+      <Separator />
+      <EnumLoader name="quarkus-extensions">
+        {extensions => (<DependenciesPicker.Element items={extensions as DependencyItem[]} value={{ dependencies: project.dependencies }} onChange={setDependencies} />)}
+      </EnumLoader>
+    </div>
+  );
 }
