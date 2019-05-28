2fd636edbd4ce1d1c95a9c0a7a5ebe512382fe06
ia3andy <adamevin@redhat.com>
2019-05-28 17:15:06 +0200

    fix: make build work

--- .circleci/upload_preview.sh
+++ .circleci/upload_preview.sh
@@ -20,6 +20,10 @@ DEPLOY_STORYBOOK_DOMAIN="https://fabric8-launcher-frontend-pr-${PR_NUM}-storyboo
 yarn comp:storybook:build
 yarn run surge --project ./packages/launcher-component/storybook-static --domain ${DEPLOY_STORYBOOK_DOMAIN};
 
+DEPLOY_QUARKUS_DOMAIN="https://fabric8-launcher-frontend-pr-${PR_NUM}-quarkus-app.surge.sh"
+yarn quarkus-app:build
+yarn run surge --project ./packages/quarkus-launcher-app/build --domain ${DEPLOY_QUARKUS_DOMAIN};
+
 if [ -z "$ALREADY_DEPLOYED_APP" ]
 then
   # Using the Issues api instead of the PR api
@@ -27,7 +31,7 @@ then
   # while the PR api requires that comments are made to specific files and specific commits
   GITHUB_PR_COMMENTS="https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/issues/${PR_NUM}/comments"
   echo "Adding github PR comment ${GITHUB_PR_COMMENTS}"
-  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"This PR was deployed **with mocked data** in the following URLs for your appreciation:\n- Frontend: '${DEPLOY_APP_DOMAIN}'\n- Welcome App: '${DEPLOY_WELCOME_APP_DOMAIN}'\n- Storybook: '${DEPLOY_STORYBOOK_DOMAIN}'"}'
+  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"This PR was deployed **with mocked data** in the following URLs for your appreciation:\n- Frontend: '${DEPLOY_APP_DOMAIN}'\n- Welcome App: '${DEPLOY_WELCOME_APP_DOMAIN}'\n- Storybook: '${DEPLOY_STORYBOOK_DOMAIN}'\n- Quarkus: '${DEPLOY_QUARKUS_DOMAIN}'"}'
 else
   echo "Already deployed ${DEPLOY_STORYBOOK_DOMAIN}"
 fi
