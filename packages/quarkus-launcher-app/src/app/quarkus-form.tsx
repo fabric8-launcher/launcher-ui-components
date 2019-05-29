ff6e48414c1b7cd8cf70ea5f23f2fb60cd3e14b4
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-05-29 15:06:38 +0200

    added hot key alt - enter

--- packages/quarkus-launcher-app/src/app/quarkus-form.tsx
+++ packages/quarkus-launcher-app/src/app/quarkus-form.tsx
@@ -1,6 +1,7 @@
 import { DependenciesPicker, EnumLoader, MavenSettingsPicker, DependencyItem, Separator } from 'launcher-component';
 import React, { useState, Fragment } from 'react';
 import { Button } from '@patternfly/react-core';
+import { HotKeys } from 'react-hotkeys';
 import { DependencyListPicker } from './dependency-list-picker';
 
 interface QuarkusFormProps {
@@ -42,54 +43,59 @@ export function QuarkusForm(props: QuarkusFormProps) {
   const setDependencies = (val: { dependencies: string[] }) => setProject((prev) => ({ ...prev, dependencies: val.dependencies }));
   return (
     <div className="quarkus-form-container">
-      <div className="brand-image" />
-      <div className="row">
-        <div className="header">
-          <h3>Project Metadata</h3>
+      <HotKeys style={{outline: 0}}
+        keyMap={{ save: 'alt+enter' }}
+        handlers={{ save: () => props.onSave(project) }}
+      >
+        <div className="brand-image" />
+        <div className="row">
+          <div className="header">
+            <h3>Project Metadata</h3>
+          </div>
+          <div className="form">
+            <MavenSettingsPicker.Element value={project.metadata} onChange={setMetadata} />
+          </div>
         </div>
-        <div className="form">
-          <MavenSettingsPicker.Element value={project.metadata} onChange={setMetadata} />
+        <div className="row">
+          <div className="header"></div>
+          <div className="form">
+            <Separator />
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="header"></div>
-        <div className="form">
-          <Separator />
+        <div className="row">
+          <EnumLoader name="quarkus-extensions">
+            {extensions => (
+              <Fragment>
+                <div className="header">
+                  <h3>Extensions</h3>
+                  <Button variant="link" onClick={() => setOpen(true)}>See all</Button>
+                  <DependencyListPicker
+                    isOpen={open}
+                    close={close}
+                    extensions={extensions as DependencyItem[]}
+                    value={{ dependencies: project.dependencies }}
+                    onChange={setDependencies}
+                  />
+                </div>
+                <div className="form">
+                  <DependenciesPicker.Element
+                    items={extensions as DependencyItem[]}
+                    value={{ dependencies: project.dependencies }}
+                    onChange={setDependencies}
+                    placeholder="RESTEasy, Hibernate ORM, Web..."
+                  />
+                </div>
+              </Fragment>
+            )}
+          </EnumLoader>
         </div>
-      </div>
-      <div className="row">
-        <EnumLoader name="quarkus-extensions">
-          {extensions => (
-            <Fragment>
-              <div className="header">
-                <h3>Extensions</h3>
-                <Button variant="link" onClick={() => setOpen(true)}>See all</Button>
-                <DependencyListPicker
-                  isOpen={open}
-                  close={close}
-                  extensions={extensions as DependencyItem[]}
-                  value={{ dependencies: project.dependencies }}
-                  onChange={setDependencies}
-                />
-              </div>
-              <div className="form">
-                <DependenciesPicker.Element
-                  items={extensions as DependencyItem[]}
-                  value={{ dependencies: project.dependencies }}
-                  onChange={setDependencies}
-                  placeholder="RESTEasy, Hibernate ORM, Web..."
-                />
-              </div>
-            </Fragment>
-          )}
-        </EnumLoader>
-      </div>
-      <div className="row footer">
-        <div className="header"></div>
-        <div className="form">
-          <Button onClick={() => props.onSave(project)}>Generate Project - alt + ⏎</Button>
+        <div className="row footer">
+          <div className="header"></div>
+          <div className="form">
+            <Button onClick={() => props.onSave(project)}>Generate Project - alt + ⏎</Button>
+          </div>
         </div>
-      </div>
+      </HotKeys>
     </div>
   );
 }
