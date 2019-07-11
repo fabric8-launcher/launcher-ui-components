4841aa2511e14126030958943d2af3d944430d10
ia3andy <adamevin@redhat.com>
2019-07-11 17:14:23 +0200

    fix: remove app from jest config

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ const baseConfig = require('./jest.config.base');
 baseConfig.roots = [
   './packages/launcher-client/src',
   './packages/launcher-component/src',
-  './packages/launcher-app/src',
-  './packages/launcher-welcome-app/src',
 ]
 
 module.exports = baseConfig;
