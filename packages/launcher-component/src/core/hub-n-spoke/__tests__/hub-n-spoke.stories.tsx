b1c6bfbc78feef5ec78f5cd88bfaa20c1c987ada
ia3andy <adamevin@redhat.com>
2019-08-13 17:52:45 +0200

    fix: update react to 16.9.0

--- packages/launcher-component/src/core/hub-n-spoke/__tests__/hub-n-spoke.stories.tsx
+++ packages/launcher-component/src/core/hub-n-spoke/__tests__/hub-n-spoke.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import { storiesOf } from '@storybook/react';
-import { HubNSpoke } from '..';
+import { HubNSpoke } from '../index';
 import { mockItems } from './mock-items';
 
 storiesOf('Core', module)
